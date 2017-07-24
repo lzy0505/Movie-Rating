9c49bcbdca12390df4cb9a6c14a2d7c911f23f6b
VermicelliInCasserole <bliu24@jhu.edu>
2017-07-24 17:00:38 +0800

    minor change, trying to fix the access bug

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
+import requests
+
 from imdb import imdb,IMDbError,IMDbDataAccessError
 from bs4 import BeautifulSoup
-import requests
 from Queue import Queue
 from threading import Thread
 
@@ -15,18 +16,18 @@ movieID = Queue(maxsize = 500)
 
 # the time range where we search for movies
 begin_year = 2017
-end_year =2017
+end_year = 2017
 
 begin_month = 7
-end_month =7
+end_month = 7
 
 def get_ID():
     for year, month in zip(range(begin_year, end_year+1), range(begin_month, end_month+1)):
-        url=imdb_new_movie_url + ( '%d-%02d' % (year, month) )
+        url = imdb_new_movie_url + ( '%d-%02d' % (year, month) )
 
         print " - GET_ID - movie url: ", url
 
-        soup=BeautifulSoup(requests.get(url).content, "lxml")
+        soup = BeautifulSoup(requests.get(url).content, "lxml")
 
         list_item = soup.findAll(True, {'class': "list_item"})
         for item in list_item:
@@ -41,20 +42,23 @@ def get_Info():
     while True:
         try:
             m_id = movieID.get()
-            movie= imdb_access.get_movie(m_id)
+            movie = imdb_access.get_movie(m_id)
             print movie.get('title')
             movieID.task_done()
-        except IOError as e:
+        except IMDbDataAccessError:
             print ' - GET_INFO - An {} exception occured'.format(e)
-            
+            sleep(2)
+
 def thread_Init():
     for i in range(thread_number):
         t=Thread(target=get_Info)
+        t.deamon = True
         t.start()
 
 if __name__ == '__main__':
     thread_Init()
     get_ID()
+
     movieID.join()
 
     print 'Done crawling data from IMDB!'
