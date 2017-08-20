5899c1844b0a6c0c35caba63190848bce1302a87
Liu Zongyuan <liuzongyuan0505@gmail.com>
2017-08-21 00:14:04 +0800

    Completed the page showing new moives details.
    import highchart.js

--- website/database.py
+++ website/database.py
@@ -11,8 +11,14 @@ cur=None
 conn=None
 
 
-test_index=[]
-test_result=[]
+info_cols=['title','cover_url','year','runtimes','genres','color_info','director','cast_1','cast_2','cast_3','countries','languages',
+'writer','editor','cinematographer','art_direction','costume_designer','original_music','sound_mix',
+'production_companies']
+
+rating_cols=['number_of_votes_1','number_of_votes_2','number_of_votes_3',
+'number_of_votes_4','number_of_votes_5','number_of_votes_6','number_of_votes_7','number_of_votes_8',
+'number_of_votes_9','number_of_votes_10']
+
 
 
 
@@ -59,10 +65,91 @@ def get_instance_basic(table):
         return templist 
 
 
+def get_instance_details(table,id):
+    connect_to_sql()
+    movie={}
+    if table=='new_movies':
+        data=sio.loadmat('o_movieDataSet.mat')
+        distr=sio.loadmat('o_predictDistribution.mat')
+        distr['preDistribution']
+        movie_rating=[]
+        for i in data['testIndex'][0]:
+            entry=[]
+            cur.execute("SELECT id,title,cover_url,year FROM %s LIMIT %s,1;" % (table,i))
+            result = cur.fetchone()
+            #print distr['preDistribution'][j]
+
+        for keyword in info_cols:
+            cur.execute("SELECT %s FROM new_movies WHERE id= '%s'" % (keyword,str(id)))
+            result = cur.fetchone()
+            if keyword=="year" or keyword=="runtimes":
+                movie[keyword]=result[0]
+            else:
+                templist=[]
+                if result[0]=='':
+                    movie[keyword]='No information'
+                else:
+                    for value in result[0].split('$'):
+                        # if len(value.split('&'))>=2: 
+                        #     templist.append(value.split('&')[1])
+                        # else:
+                        #     templist.append(value)
+                        templist.append(value)
+                    movie[keyword]=templist                    
+            conn.commit()
+        for keyword in rating_cols:
+            cur.execute("SELECT %s FROM new_movies WHERE id= '%s'" % (keyword,str(id)))
+            result = cur.fetchone()
+            rating_sum=rating_sum+float(result[0])
+            movie_rating.append(float(result[0]))
+            conn.commit()
+        for i in xrange(0,len(movie_rating)):
+            movie[rating_cols[i]]=movie_rating[i]/rating_sum 
+        print movie
+        return movie
+
+    elif table =='future_movies':
+        distr=sio.loadmat('f_predictDistribution.mat')
+        for keyword in info_cols:
+            cur.execute("SELECT %s FROM future_movies WHERE id= '%s'" % (keyword,str(id)))
+            result = cur.fetchone()
+            conn.commit()
+            if keyword=="year" or keyword=="runtimes":
+                movie[keyword]=result[0]
+            else:
+                templist=[]
+                if result[0]=='':
+                    movie[keyword]='No information'
+                else:
+                    for value in result[0].split('$'):
+                        if len(value.split('&'))>=2: 
+                            templist.append(value.split('&')[1])
+                        else:
+                            templist.append(value)
+                        # templist.append(value)
+                    movie[keyword]=templist                    
+            conn.commit()    
+        cur.execute("SELECT id FROM future_movies;")
+        result = cur.fetchall()
+        conn.commit()
+        index=0
+        for m in result:
+            if m[0]== id:
+                break
+            index+=1
+        for i in xrange(0,10):
+            movie['f_'+rating_cols[i]]=distr['preDistribution'][index][i]
+
+        # print movie
+        return movie
+        
+
 
 
 if __name__ == '__main__':
-    get_instance_basic('new_movies')
+    get_instance_details('future_movies','5362988')
+
+
 
 
 
